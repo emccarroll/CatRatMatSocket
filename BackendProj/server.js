651d2cd3b4239f3d2143efbe37eee9c0582c4e89
Jacob Santoni <jlsantoni2@gmail.com>
2020-04-09 20:59:58 -0400

    removed whitespace

--- BackendProj/server.js
+++ BackendProj/server.js
@@ -16,7 +16,7 @@ var storage = multer.diskStorage({
     filename: function (req, file, cb) {
       cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
     }
-  })
+})
 
 var upload = multer({ 
     //dest: 'images/'
